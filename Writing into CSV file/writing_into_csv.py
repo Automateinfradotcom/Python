2dca28cd344f0d060ac923159e1a8785188872c4
Automateinfra.com <59646603+Automateinfradotcom@users.noreply.github.com>
2021-06-18 17:11:04 +0530

    Update writing_into_csv.py

--- Writing into CSV file/writing_into_csv.py
+++ Writing into CSV file/writing_into_csv.py
@@ -2,4 +2,4 @@ import csv
 
 with open('devops.csv','a', newline='') as csv_file:
     write_csv = csv.writer(csv_file,  delimiter=',')
-    write_csv.writerow(['08-08-2021','68','8888', 'ATA.com/blog8'])
+    write_csv.writerow(['08-08-2021','68','8888', 'site.com/blog8'])
