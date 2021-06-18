5d0531a3cda73908dcbbae2e6d9abcd0f8c08bc7
Automateinfra.com <59646603+Automateinfradotcom@users.noreply.github.com>
2021-06-18 17:10:42 +0530

    Add files via upload

---
+++ Writing into CSV file/writing_into_csv.py
@@ -0,0 +1,5 @@
+import csv
+
+with open('devops.csv','a', newline='') as csv_file:
+    write_csv = csv.writer(csv_file,  delimiter=',')
+    write_csv.writerow(['08-08-2021','68','8888', 'ATA.com/blog8'])
